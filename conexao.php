b5ae609f417d8c7389bc498ba2a0337a69db46bf
Mauricio-TW <mauriciothumwelter@gmail.com>
2023-12-19 20:27:55 -0300

    aaaa

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php 
     $banco = mysqli_connect("localhost","root","","sistema_login",);
     if(!$banco){
-         echo "Não foi possivel conectar com o BD.<br>Causa:".mysqli_connect_error();
+         //echo "Não foi possivel conectar com o BD.<br>Causa:".mysqli_connect_error();
     } else {
-         echo "Conexão com o BD ocorreu normalmente.<br>";
+         //echo "Conexão com o BD ocorreu normalmente.<br>";
     }
 ?>
--- index.php
+++ index.php
@@ -32,7 +32,7 @@ mysqli_close($banco);
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" href="Style.css">
         <title>Sistema de Login</title>
-        <link rel="icon" href="/assets/img/imgSite/lamborghini logo1.png" type="Logo">
+        <link rel="icon" href="/assets/imgSite/lamborghini logo1.png" type="Logo">
 </head>
 
 <body>
@@ -42,8 +42,8 @@ mysqli_close($banco);
     <!--Header-->
     <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
         <div class="container-fluid">
-          <a class="navbar-brand" href="index.html">
-            <img src="/assets/img/imgSite/lamborghini logo1.png" width="100" height="100" alt="Logo"></a>
+          <a class="navbar-brand" href="index.php">
+            <img src="/assets/imgSite/lamborghini logo1.png" width="100" height="100" alt="Logo"></a>
           </a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#mynavbar">
             <span class="navbar-toggler-icon"></span>
